0ab96e8bc2798f6c4b2ae1847044b18f6dcd5eec
Heba tqeshm <hebatm29@gmail.com>
2026-06-24 00:12:34 +0300

    footeerrrr

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,14 +8,14 @@ const Footer = () => {
     <footer>
         <a href="#"className='footer_logo'>Heba Allah</a>
 
-        <ul className='permalinks'>
+        {/* <ul className='permalinks'>
             <li> <a href="#home">Home</a> </li>
             <li> <a href="#about">About</a> </li>
             <li> <a href="#skills">Skills</a> </li>
             <li> <a href="#services">Services</a> </li>
             <li> <a href="#projects">Projects</a> </li>
             <li> <a href="#contact">Contact</a> </li>
-        </ul>
+        </ul> */}
         <div className="footer_socials">
             <a href="https://www.linkedin.com/in/hebah-allah-mohamed-36bb97283/" target='_blank'><FaLinkedin/> </a>
             <a href="https://github.com/HeBaMo7amed" target='_blank'> <FaGithub/></a>
